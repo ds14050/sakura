5863906835570145cdb6341cd82afe80ce133a33
ds14050 <ds14050@vvvvvv.sakura.ne.jp>
2018-11-25 18:32:08 +0900

    new subroutine: Set_BASENAME

--- zipArtifacts.bat
+++ zipArtifacts.bat
@@ -22,121 +22,13 @@ if "%configuration%" == "Release" (
 )
 
 if "%platform%" == "x64" (
-	set ALPHA=1
+	set ALPHA=alpha
 ) else (
-	set ALPHA=0
+	set ALPHA=
 )
 
 set ZIP_CMD=%~dp0tools\zip\zip.bat
 set LIST_ZIP_CMD=%~dp0tools\zip\listzip.bat
-
-@rem ----------------------------------------------------------------
-@rem prepare environment variable
-@rem ----------------------------------------------------------------
-@echo checking APPVEYOR_ACCOUNT_NAME %APPVEYOR_ACCOUNT_NAME%
-set BUILD_ACCOUNT=
-if "%APPVEYOR_ACCOUNT_NAME%" == "sakuraeditor" (
-	set BUILD_ACCOUNT=
-) else if "%APPVEYOR_ACCOUNT_NAME%" == "" (
-	set BUILD_ACCOUNT=
-) else (
-	set BUILD_ACCOUNT=%APPVEYOR_ACCOUNT_NAME%
-)
-
-@echo checking APPVEYOR_BUILD_NUMBER %APPVEYOR_BUILD_NUMBER%
-if not "%APPVEYOR_BUILD_NUMBER%" == "" (
-	set BUILD_NUMBER=build%APPVEYOR_BUILD_NUMBER%
-) else (
-	set BUILD_NUMBER=buildLocal
-)
-
-@echo checking APPVEYOR_REPO_TAG_NAME %APPVEYOR_REPO_TAG_NAME%
-if not "%APPVEYOR_REPO_TAG_NAME%" == "" (
-	@rem replace '/' with '_'
-	set TEMP_NAME1=!APPVEYOR_REPO_TAG_NAME:/=_!
-	@echo TEMP_NAME1 = !TEMP_NAME1!
-	
-	@rem replace ' ' with '_'
-	set TEMP_NAME2=!TEMP_NAME1: =_!
-	@echo TEMP_NAME2 = !TEMP_NAME2!
-
-	@rem replace ' ' with '_'
-	set TAG_NAME=tag-!TEMP_NAME2!
-	@echo TAG_NAME = !TEMP_NAME2!
-)
-
-@echo checking APPVEYOR_PULL_REQUEST_NUMBER %APPVEYOR_PULL_REQUEST_NUMBER%
-if not "%APPVEYOR_PULL_REQUEST_NUMBER%" == "" (
-	set PR_NAME=PR%APPVEYOR_PULL_REQUEST_NUMBER%
-)
-
-@echo checking APPVEYOR_REPO_COMMIT %APPVEYOR_REPO_COMMIT%
-if not "%APPVEYOR_REPO_COMMIT%" == "" (
-	set SHORTHASH=%APPVEYOR_REPO_COMMIT:~0,8%
-)
-
-if "%ALPHA%" == "1" (
-	set RELEASE_PHASE=alpha
-) else (
-	set RELEASE_PHASE=
-)
-
-@rem ----------------------------------------------------------------
-@rem build BASENAME
-@rem ----------------------------------------------------------------
-set BASENAME=sakura
-
-@echo adding BUILD_ACCOUNT
-if not "%BUILD_ACCOUNT%" == "" (
-	set BASENAME=%BASENAME%-%BUILD_ACCOUNT%
-)
-@echo BASENAME = %BASENAME%
-
-@echo adding TAG_NAME
-if not "%TAG_NAME%" == "" (
-	set BASENAME=%BASENAME%-%TAG_NAME%
-)
-@echo BASENAME = %BASENAME%
-
-@echo adding PR_NAME
-if not "%PR_NAME%" == "" (
-	set BASENAME=%BASENAME%-%PR_NAME%
-)
-@echo BASENAME = %BASENAME%
-
-@echo adding BUILD_NUMBER
-if not "%BUILD_NUMBER%" == "" (
-	set BASENAME=%BASENAME%-%BUILD_NUMBER%
-)
-@echo BASENAME = %BASENAME%
-
-@echo adding SHORTHASH
-if not "%SHORTHASH%" == "" (
-	set BASENAME=%BASENAME%-%SHORTHASH%
-)
-@echo BASENAME = %BASENAME%
-
-@echo adding platform and configuration
-set BASENAME=%BASENAME%-%platform%-%configuration%
-@echo BASENAME = %BASENAME%
-
-@echo adding RELEASE_PHASE
-if not "%RELEASE_PHASE%" == "" (
-	set BASENAME=%BASENAME%-%RELEASE_PHASE%
-)
-@echo BASENAME = %BASENAME%
-
-@rem ---------------------- BASENAME ---------------------------------
-@rem "sakura"
-@rem BUILD_ACCOUNT: (option) APPVEYOR_ACCOUNT_NAME
-@rem TAG_NAME     : (option) tag Name
-@rem PR_NAME      : (option) PRxxx (xxx is a PR number)
-@rem BUILD_NUMBER : (option) buildYYY or "buildLocal" (YYY is build number)
-@rem SHORTHASH    : (option) hash or "buildLocal" (hash is leading 8 charactors)
-@rem platform     : Platform ("Win32" or "x64")
-@rem configuration: Configuration ("Debug" or "Release")
-@rem RELEASE_PHASE: (option) "alpha" (x64 build only)
-@rem ----------------------------------------------------------------
 
 @rem ----------------------------------------------------------------
 @rem build WORKDIR
@@ -244,7 +136,7 @@ if exist "%HASHFILE%" (
 )
 
 copy /Y installer\warning.txt   %WORKDIR%\
-if "%ALPHA%" == "1" (
+if defined ALPHA (
 	copy /Y installer\warning-alpha.txt   %WORKDIR%\
 )
 @rem temporally disable to zip all files to a file to workaround #514.
@@ -253,7 +145,7 @@ if "%ALPHA%" == "1" (
 call %ZIP_CMD%       %OUTFILE_LOG%  %WORKDIR_LOG%
 
 @rem copy text files for warning after zipping %OUTFILE% because %WORKDIR% is the parent directory of %WORKDIR_EXE% and %WORKDIR_INST%.
-if "%ALPHA%" == "1" (
+if defined ALPHA (
 	copy /Y installer\warning-alpha.txt   %WORKDIR_EXE%\
 	copy /Y installer\warning-alpha.txt   %WORKDIR_INST%\
 )
@@ -276,6 +168,72 @@ if exist "%WORKDIR_ASM%" (
 	rmdir /s /q "%WORKDIR_ASM%"
 )
 
+exit /b 0
+
+@rem ---------------------- BASENAME ---------------------------------
+@rem "sakura"
+@rem BUILD_ACCOUNT (option)
+@rem TAG_NAME      (option) "tag-" is prefixed.
+@rem PR_NUMBER     (option) "PR" is prefixed.
+@rem {BUILD_NUMBER|"Local"} "build" is prefixed.
+@rem SHORTHASH     (option) SHORTHASH is leading 8 charactors
+@rem PLATFORM
+@rem CONFIGURATION
+@rem ALPHA         (x64 build only)
+@rem ----------------------------------------------------------------
+
+:Set_BASENAME
+	setlocal ENABLEDELAYEDEXPANSION
+
+	set BUILD_ACCOUNT=%APPVEYOR_ACCOUNT_NAME%
+	if "%BUILD_ACCOUNT%" == "sakuraeditor" set BUILD_ACCOUNT=
+
+	set TAG_NAME=%APPVEYOR_REPO_TAG_NAME%
+	if defined TAG_NAME set TAG_NAME=tag-%TAG_NAME%
+	call :ReplaceForbiddenPathChars TAG_NAME
+
+	set PR_NUMBER=%APPVEYOR_PULL_REQUEST_NUMBER%
+	if defined PR_NUMBER set PR_NUMBER=PR%PR_NUMBER%
+
+	set BUILD_NUMBER=%APPVEYOR_BUILD_NUMBER%
+	if defined BUILD_NUMBER set BUILD_NUMBER=Local
+	set BUILD_NUMBER=build%BUILD_NUMBER%
+
+	set SHORTHASH=%APPVEYOR_REPO_COMMIT%
+	if defined SHORTHASH set SHORTHASH=%SHORTHASH:~0,8%
+
+	rem PLATFORM
+
+	rem CONFIGURATION
+
+	rem ALPHA
+
+	set BASENAME=sakura
+	for %%V in (
+		BUILD_ACCOUNT TAG_NAME PR_NUMBER BUILD_NUMBER
+		SHORTHASH PLATFORM CONFIGURATION ALPHA
+	) do (
+		echo %%V=!%%V!
+		if defined %%V set BASENAME=!BASENAME!-!%%V!
+	)
+
+	endlocal & set BASENAME=%BASENAME%
+exit /b 0
+
+@rem '/' -> '_'
+@rem ' ' -> '_'
+:ReplaceForbiddenPathChars
+	setlocal ENABLEDELAYEDEXPANSION
+
+	set VAR=%~1
+	if not defined VAR exit /b 1
+	set VAL=!%VAR%!
+	if not defined VAL exit /b 1
+
+	set VAL=%VAL:/=_%
+	set VAL=%VAL: =_%
+
+	endlocal & set %VAR%=%VAL%
 exit /b 0
 
 @rem ------------------------------------------------------------------------------
